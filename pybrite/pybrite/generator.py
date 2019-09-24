187c0b1c58329e5d4b16d18b9ee26c123a71071b
Caio Dadauto <caiodadauto@gmail.com>
2019-09-24 16:31:51 -0300

    Add option to work with regression problems

--- pybrite/pybrite/generator.py
+++ pybrite/pybrite/generator.py
@@ -7,7 +7,7 @@ from .utils import add_shortest_path, graph_to_input_target
 from .paths import GRAPH_BRITE_PATH, BRITE_CONFIG_PATH, SEED_BRITE_PATH, LAST_SEED_BRITE_PATH
 
 
-def graph_batch_generator(n_batch, interval_node, interval_m=(2, 2), random_state=None, input_fields=None, target_fields=None, global_field=None):
+def graph_batch_generator(n_batch, interval_node, interval_m=(2, 2), random_state=None, is_classification=True, input_fields=None, target_fields=None, global_field=None):
     min_n, max_n = interval_node
     min_m, max_m = interval_m
     random_state = random_state if random_state else np.random.RandomState()
@@ -19,7 +19,9 @@ def graph_batch_generator(n_batch, interval_node, interval_m=(2, 2), random_stat
             m = random_state.choice(range(min_m, max_m + 1))
             graph = get_graph(n, m, random_state=random_state)
             di_graph, target_node = add_shortest_path(graph, random_state=random_state)
-            input_graph, target_graph = graph_to_input_target(di_graph, target_node, input_fields=input_fields, target_fields=target_fields, global_field=global_field)
+            input_graph, target_graph = graph_to_input_target(
+                di_graph, target_node, is_classification=is_classification, input_fields=input_fields,
+                target_fields=target_fields, global_field=global_field)
             input_batch.append(input_graph)
             target_batch.append(target_graph)
         yield input_batch, target_batch
