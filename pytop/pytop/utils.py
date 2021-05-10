8fa1fa031d58d02456297628c005de3b3c765ec7
Caio Dadauto <caiodadauto@gmail.com>
2021-05-10 11:10:20 -0300

    apply scaler to edge and node features

--- pytop/pytop/utils.py
+++ pytop/pytop/utils.py
@@ -62,10 +62,12 @@ def add_shortest_path(graph, random_state):
         if node != end:
             solution_edges.extend(list(pairwise(path[end])))
         min_distance.append(
-            (node, dict(min_distance_to_end=distance[end], hops_to_end=len(path[end])))
+            (node, dict(
+                min_distance_to_end=distance[end], hops_to_end=len(path[end])))
         )
     digraph.add_nodes_from(min_distance)
-    digraph.add_edges_from(set_diff(digraph.edges(), solution_edges), solution=False)
+    digraph.add_edges_from(
+        set_diff(digraph.edges(), solution_edges), solution=False)
     digraph.add_edges_from(solution_edges, solution=True)
     digraph.graph["target"] = end
     return digraph
@@ -86,7 +88,8 @@ def graph_to_input_target(
         return np.hstack([np.array(attr[field], dtype=dtype) for field in fields])
 
     input_node_fields = (
-        input_fields["node"] if input_fields and "node" in input_fields else ("ip",)
+        input_fields["node"] if input_fields and "node" in input_fields else (
+            "ip",)
     )
     input_edge_fields = (
         input_fields["edge"]
@@ -104,50 +107,53 @@ def graph_to_input_target(
         else ("solution",)
     )
 
-    input_graph = graph.copy()
-    target_graph = graph.copy()
+    _graph = graph.copy()
+    if scaler is not None:
+        d_distance = nx.get_edge_attributes(_graph, "distance")
+        d_pos = nx.get_node_attributes(_graph, "pos")
+        all_distance = list(d_distance.values())
+        all_pos = list(d_pos.values())
+        nx.set_edge_attributes(_graph, dict(
+            zip(d_distance, scaler(all_distance))), "distance")
+        nx.set_node_attributes(_graph, dict(
+            zip(d_pos, scaler(all_pos))), "pos")
+        raw_features = (d_distance, d_pos)
+    else:
+        raw_features = None
+    input_graph = _graph.copy()
+    target_graph = _graph.copy()
 
-    end = graph.graph["target"]
-    for node_index, node_feature in graph.nodes(data=True):
+    end = _graph.graph["target"]
+    for node_index, node_feature in _graph.nodes(data=True):
         if node_index == end:
             end_node = node_feature[global_field if global_field else "ip"].astype(
                 dtype
             )
         input_graph.add_node(
-            node_index, features=create_feature(node_feature, input_node_fields, dtype)
+            node_index, features=create_feature(
+                node_feature, input_node_fields, dtype)
         )
         target_graph.add_node(
-            node_index, features=create_feature(node_feature, target_node_fields, dtype)
+            node_index, features=create_feature(
+                node_feature, target_node_fields, dtype)
         )
 
-    input_edge_features = []
-    for feature in input_edge_fields:
-        edge_feature = np.array(
-            list(nx.get_edge_attributes(graph, feature).values()),
-            dtype=dtype,
-        )
-        input_edge_features.append(edge_feature)
-    input_edge_features = np.hstack(input_edge_features)
-    if input_edge_features.ndim == 1:
-        input_edge_features = input_edge_features.reshape(-1, 1)
-    raw_input_edge_features = input_edge_features.copy()
-    if scaler is not None:
-        input_edge_features = scaler(input_edge_features)
-    for i, (sender, receiver) in enumerate(graph.edges()):
+    for sender, receiver, edge_feature in _graph.edges(data=True):
         input_graph.add_edge(
             sender,
             receiver,
-            features=input_edge_features[i],
+            features=create_feature(
+                edge_feature, input_edge_fields, dtype),
         )
-    for sender, receiver, features in graph.edges(data=True):
         if bidim_solution:
             target_edge = to_one_hot(
-                create_feature(features, target_edge_fields, dtype).astype(np.int32), 2
+                create_feature(edge_feature, target_edge_fields,
+                               dtype).astype(np.int32), 2
             )[0]
         else:
-            target_edge = create_feature(features, target_edge_fields, dtype)
+            target_edge = create_feature(edge_feature, target_edge_fields, dtype)
         target_graph.add_edge(sender, receiver, features=target_edge)
 
     input_graph.graph["features"] = end_node
     target_graph.graph["features"] = end_node
-    return input_graph, target_graph, raw_input_edge_features
+    return input_graph, target_graph, raw_features
